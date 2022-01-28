4552a1df49ceab3bf5a5a4aee71cd1a1b6e834b8
Dan Nguyen <77303944+dannguyen-1004@users.noreply.github.com>
2022-01-28 05:11:04 -0600

    Final

--- About/script.js
+++ About/script.js
@@ -9,3 +9,18 @@ function goUp(){
     document.getElementById("after").style.display = "none";
     document.getElementById("container").style.display = "none";
 }
+
+var coll = document.getElementsByClassName("collap");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}

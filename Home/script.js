bb21733d521a7a84aafba3cb4709ed3ab92f4992
Dan Nguyen <77303944+dannguyen-1004@users.noreply.github.com>
2022-04-09 14:54:49 -0500

    i forgot

--- Home/script.js
+++ Home/script.js
@@ -1,4 +1,5 @@
 
+
 function dropDown(){
     document.getElementById("before").style.display = "none";
     document.getElementById("after").style.display = "block";
@@ -10,48 +11,72 @@ function goUp(){
     document.getElementById("container").style.display = "none";
 }
 
-/*
-var tl = new TimelineMax({onUpdate: updatePercentage});
-const controller = new ScrollMagic.Controller();
-
-tl.to("#tree",  2, {y: -55, opacity: 1});
-
-const scene = new ScrollMagic.Scene({
-    triggerElement: "#fifth",
-    triggerHook: "onLeave",
-    duration: "70%"
-})
-
-.setPin("#fifth")
-.setTween(tl)
-    .addTo(controller);
-
-function updatePercentage(){
-    tl.progress();
+//THIRD
+if(window.innerWidth >= 600){
+    gsap.to(".pin",{
+        scrollTrigger:{
+            trigger: ".pin",
+            start: "center center",
+            scrub: true,
+            pin:".pin",
+            end: "+=300"
+        },
+    })
+    gsap.to(".pin2",{
+        scrollTrigger:{
+            trigger: ".pin",
+            start: "center center",
+            scrub: true,
+            pin:".pin2",
+            end: "+=300"
+        },
+    })
 }
-*/
-
-let controller = new ScrollMagic.Controller();
-let timeLine = new TimelineMax();
-
-timeLine
-.to('#tree', .5, {y: -200})
-.to('.rock', 3, {y: -300}, '-=3')
-.fromTo('.background', 3, {y:-50},{y:-50}, '-=3')
-.to('.body', 5, {y: -600}, '-=3')
-.to('.footer', 5, {y: -600}, '-=3')
+console.log(window.innerWidth)
 
 
-
-.from('.quote', 8, {opacity: 0}, '-=2')
-.from('.downNav', 8, {opacity: 0}, '-=8')
-.from('.tips', 1, {opacity: 0}, '-=8')
-
-let scene1 = new ScrollMagic.Scene({
-    triggerElement: "section",
-    duration: "70%",
-    triggerHook: 0,
+//FOURTH
+gsap.to("#fourth",{
+    scrollTrigger:{
+        trigger: "#fourth",
+        start: "top top",
+        scrub: true,
+    },
 })
-
-.setTween(timeLine)
-.addTo(controller)
+gsap.to("#back",{
+    scrollTrigger:{
+        trigger: "#fourth",
+        start: "top center",
+        scrub: true,
+        duration:20
+    },
+    y:250,
+    scale:1.5
+})
+gsap.to("#white-top",{
+    scrollTrigger:{
+        trigger: "#fourth",
+        start: "top center",
+        scrub: true,
+        duration:20
+    },
+    scale: 1.5, 
+})
+gsap.to("#white-top",{
+    scrollTrigger:{
+        trigger: ".sun",
+        start: "top center",
+        scrub: true,
+        duration:20
+    },
+    y: 250, 
+})
+gsap.to("#front",{
+    scrollTrigger:{
+        trigger: "#fourth",
+        start: "bottom bottom",
+        scrub: true,
+        duration:30
+    },
+    y: 200
+})
